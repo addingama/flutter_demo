a329abb914460c0617e30b04b25eb475fd3b482b
Addin Gama Bertaqwa <punyagama@gmail.com>
2018-04-21 07:12:46 +0800

    add action button

--- lib/main.dart
+++ lib/main.dart
@@ -23,11 +23,17 @@ class RandomWordsState extends State<RandomWords> {
   final _saved = new Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
+  void _pushSaved() {
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
